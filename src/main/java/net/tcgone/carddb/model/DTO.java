b537350156148d64e2dde6d48643eceb13fa7162
axpendix <axpendix@hotmail.com>
2019-02-05 09:44:50 +0100

    Minor improvements

--- src/main/java/net/tcgone/carddb/model/DTO.java
+++ src/main/java/net/tcgone/carddb/model/DTO.java
@@ -3,39 +3,39 @@ package net.tcgone.carddb.model;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 public class DTO {
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
-    @Data @AllArgsConstructor
+    @Data @AllArgsConstructor @NoArgsConstructor
     public static class CardLite {
-        String id;
-        String name;
-        String imageUrl;
-        Integer quantity; //for inventory & deck
+        private String id;
+        private String name;
+        private String imageUrl;
+        private Integer quantity; //for inventory & deck
     }
     @JsonInclude(JsonInclude.Include.NON_NULL)
-    @Data @AllArgsConstructor
+    @Data @AllArgsConstructor @NoArgsConstructor
     public static class SearchResults {
-        List<CardLite> data;
-        Integer totalPages;
-        Integer totalElements;
-        String responseTime;
+        private List<CardLite> data;
+        private Integer totalPages;
+        private Integer totalElements;
+        private String responseTime;
     }
-    @Data
-    @AllArgsConstructor
+    @Data @AllArgsConstructor @NoArgsConstructor
     public static class SetLite {
-        String id;
-        String name;
-        String abbr;
+        private String id;
+        private String name;
+        private String abbr;
     }
-    @Data @AllArgsConstructor
+    @Data @AllArgsConstructor @NoArgsConstructor
     public static class FormatLite {
-        String id;
-        String name;
-        String description;
+        private String id;
+        private String name;
+        private String description;
     }
 
 }
