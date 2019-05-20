81959be1222664371d81f3bfe7eea49d9de38151
axpendix <axpendix@hotmail.com>
2019-05-20 17:41:33 +0200

    Add Deck model WIP

---
+++ src/main/java/net/tcgone/carddb/model/Deck.java
@@ -0,0 +1,31 @@
+package net.tcgone.carddb.model;
+
+import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author axpendix@hotmail.com
+ * @since 20.05.2019
+ */
+@Data
+public class Deck {
+    private String id; // external id
+    private String internalId; // internal mongo id, can be different for legacy decks
+    private String seoName;
+    private String name;
+    private String format; // main format
+    private String description;
+    private String creatorId;
+    private String creatorUsername; // not serialized
+    private Map<String, Integer> contentsEnumId; // contents with enumId (legacy)
+    private Map<String, Integer> contents; // contents with new id
+    private Date lastUpdated;
+    private List<String> tags; // theme, public, private, draft, career, list
+    private List<String> colors; // fire, grass, psychic
+    private List<String> tiers; // tournament, tier1, tier2, tier3, experimental, other, fun
+    private List<String> variants; // unholy paladin, haymaker
+    private List<String> formats; // all valid formats that this can be played in
+}
---
+++ src/main/java/net/tcgone/carddb/model/DeckStats.java
@@ -0,0 +1,19 @@
+package net.tcgone.carddb.model;
+
+import lombok.Data;
+
+/**
+ * @author axpendix@hotmail.com
+ * @since 20.05.2019
+ */
+@Data
+public class DeckStats {
+	private String id; // mongo id of the deck
+	private java.util.Set<String> upVoteIds;
+	private java.util.Set<String> downVoteIds;
+	private int voteScore;
+	private int timesWon; //?
+	private int timesLost; //?
+	private double rating; //?
+	private int timesUsed;
+}
