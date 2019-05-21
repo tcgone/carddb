e3b068a8dd4b2179ca760bdde97798ffde030fc3
axpendix <axpendix@hotmail.com>
2019-05-21 19:44:40 +0200

    WIP Deck

--- src/main/java/net/tcgone/carddb/model/Deck.java
+++ src/main/java/net/tcgone/carddb/model/Deck.java
@@ -2,6 +2,8 @@ package net.tcgone.carddb.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -15,12 +17,16 @@ public class Deck {
     private String id; // external id
     private String oldId; // old internal mongo id, stored for legacy decks
     private String seoName; // generated seo name except id
+    @NotBlank
     private String name;
+    @NotBlank
     private String format; // main format
     private String description;
     private String creatorId;
     private String creatorUsername; // not serialized
+    @NotNull
     private Map<String, Integer> contentsEnumId; // contents with enumId (legacy)
+    @NotNull
     private Map<String, Integer> contents; // contents with new id
     private Date lastUpdated;
     private List<String> tags; // theme, public, private, draft, career, list
@@ -28,4 +34,6 @@ public class Deck {
     private List<String> tiers; // tournament, tier1, tier2, tier3, experimental, other, fun
     private List<String> variants; // unholy paladin, haymaker
     private List<String> formats; // all valid formats that this can be played in
+    private boolean ready;
+    private String error;
 }
