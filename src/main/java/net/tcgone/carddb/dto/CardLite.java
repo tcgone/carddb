9e1b8ea506a49276bd52931c43b4505c5430fdeb
axpendix <axpendix@hotmail.com>
2019-09-23 17:50:17 +0200

    Improving of Lucene search & analysis functionality and DTO refactorings

---
+++ src/main/java/net/tcgone/carddb/dto/CardLite.java
@@ -0,0 +1,17 @@
+package net.tcgone.carddb.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CardLite {
+	private String id;
+	private String name;
+	private String imageUrl;
+	private Integer quantity; //for inventory & deck
+}
