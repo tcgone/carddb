59b9b358543610b3ac3e61626bc65a973ec682eb
axpendix <axpendix@hotmail.com>
2019-11-16 20:21:49 +0100

    Type refactoring

--- src/main/java/net/tcgone/carddb/model/Type.java
+++ src/main/java/net/tcgone/carddb/model/Type.java
@@ -1,4 +1,4 @@
-package net.tcgone.carddb.model.experimental;
+package net.tcgone.carddb.model;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
