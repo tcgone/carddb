99c4be84d8cc697daef151ca899fe6e5c5f16f54
axpendix <axpendix@hotmail.com>
2019-11-17 00:45:38 +0100

    Add displayLabel and searchLabels properties to CardType

--- src/main/java/net/tcgone/carddb/model/CardType.java
+++ src/main/java/net/tcgone/carddb/model/CardType.java
@@ -1,9 +1,10 @@
 package net.tcgone.carddb.model;
 
-import org.apache.commons.text.WordUtils;
+import com.google.common.collect.ImmutableList;
 
 import java.io.Serializable;
 import java.util.Comparator;
+import java.util.List;
 
 /**
  * Experimental
@@ -12,76 +13,74 @@ import java.util.Comparator;
  */
 public enum CardType {
 
-  POKEMON(10),
-  ENERGY(20),
-  TRAINER(30),
+  POKEMON(10, "Pokémon", "pokemon", "pokémon"),
+  ENERGY(20, "Energy", "energy"),
+  TRAINER(30, "Trainer", "trainer"),
 
-  MEGA_POKEMON(-6),
-  LEVEL_UP(-5),
-  BREAK(-4),
-  STAGE2(-2),
-  STAGE1(-1),
-  EVOLUTION(0),
-  RESTORED(4),
-  BASIC(7),
-  BABY(8),
-  LEGEND(9),
+  MEGA_POKEMON(-6, "MEGA", "mega"),
+  LEVEL_UP(-5, "LV.X", "lvx", "levelup"),
+  BREAK(-4, "BREAK", "break"),
+  STAGE2(-2, "Stage 2", "stage2", "s2"),
+  STAGE1(-1, "Stage 1", "stage1", "s1"),
+  EVOLUTION(0, "Evolution", "evolution"),
+  RESTORED(4, "Restored", "restored"),
+  BASIC(7, "Basic", "basic"),
+  BABY(8, "Baby", "baby"),
+  LEGEND(9, "LEGEND", "legend"),
 
-  BASIC_ENERGY(18),
-  SPECIAL_ENERGY(19),
-  ITEM(23),
-  SUPPORTER(24),
-  STADIUM(25),
-  POKEMON_TOOL(26),
-  TECHNICAL_MACHINE(27),
-  FLARE(28),
-  ROCKETS_SECRET_MACHINE(29),
+  BASIC_ENERGY(18, "Basic Energy", "basic-energy", "basic"),
+  SPECIAL_ENERGY(19, "Special Energy", "special-energy", "special"),
+  ITEM(23, "Item", "item"),
+  SUPPORTER(24, "Supporter", "supporter"),
+  STADIUM(25, "Stadium", "stadium"),
+  POKEMON_TOOL(26, "Pokémon Tool", "tool"),
+  TECHNICAL_MACHINE(27, "Technical Machine", "technical-machine", "tm"),
+  FLARE(28, "FLARE", "flare"),
+  ROCKETS_SECRET_MACHINE(29, "Secret Machine", "secret-machine"),
 
-  TAG_TEAM(93),
-  ULTRA_BEAST(94),
-  PRISM_STAR(95),
-  POKEMON_GX(96, "Pokémon-GX"),
-  POKEMON_PRIME(97),
-  POKEMON_STAR(98),
-  POKEMON_EX(99, "Pokémon-EX"), //UPPERCASE
-  EX(100, "Pokémon-ex"), //LOWERCASE
-  FOSSIL(101),
-  TEAM_MAGMA(102),
-  TEAM_AQUA(103),
-  OWNERS_POKEMON(104),
-  DARK_POKEMON(105),
-  LIGHT_POKEMON(106),
-  SHINING_POKEMON(107),
-  TEAM_PLASMA(108),
-  ACE_SPEC(109),
-  HAS_ANCIENT_TRAIT(110),
+  TAG_TEAM(93, "TAG TEAM", "tag-team"),
+  ULTRA_BEAST(94, "Ultra Beast", "ultra-beast"),
+  PRISM_STAR(95, "Prism Star", "prism-star"),
+  POKEMON_GX(96, "Pokémon-GX", "pokemon-gx", "gx"),
+  POKEMON_PRIME(97, "Pokémon Prime", "pokemon-prime", "prime"),
+  POKEMON_STAR(98, "Pokémon Star", "pokemon-star", "star"),
+  POKEMON_EX(99, "Pokémon-EX", "pokemon-ex", "ex"), //UPPERCASE
+  EX(100, "Pokémon-ex", "legacy-ex", "ex"), //LOWERCASE
+  FOSSIL(101, "Fossil", "fossil"),
+  TEAM_MAGMA(102, "Team Magma", "team-magma", "magma"),
+  TEAM_AQUA(103, "Team Aqua", "team-aqua", "aqua"),
+  OWNERS_POKEMON(104, "Owner's", "owners"),
+  DARK_POKEMON(105, "Dark", "dark"),
+  LIGHT_POKEMON(106, "Light", "light"),
+  SHINING_POKEMON(107, "Shining", "shining"),
+  TEAM_PLASMA(108, "Team Plasma", "team-plasma", "plasma"),
+  ACE_SPEC(109, "ACE Spec", "ace-spec"),
+  HAS_ANCIENT_TRAIT(110, "Has Ancient Trait", "has-ancient-trait"),
 
-  NOT_IMPLEMENTED(201),
+  NOT_IMPLEMENTED(201, "Not Implemented", "not-implemented"),
 
   ;
 
   private final int priority;
-  private final String label;
+  private final List<String> searchLabels;
+  private final String displayLabel;
 
-  CardType(int priority) {
-    this(priority, null);
-  }
-
-  CardType(int priority, String label) {
+  CardType(int priority, String displayLabel, String... searchLabels) {
     this.priority = priority;
-    if (label != null) {
-      this.label = label;
-    } else {
-      this.label = WordUtils.capitalizeFully(name().replace("_", " ")).replace("Pokemon", "Pokémon");
-    }
+    this.displayLabel = displayLabel;
+    this.searchLabels = ImmutableList.copyOf(searchLabels);
   }
 
   public int getPriority() {
     return priority;
   }
 
-  public String getLabel() {
-    return label;
+  public String getDisplayLabel() {
+    return displayLabel;
+  }
+
+  public List<String> getSearchLabels() {
+    return searchLabels;
   }
 
   public static class CardTypeComparator implements Comparator<CardType>, Serializable {
