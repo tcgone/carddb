724429f7760d655134a21fe4121ac9f95381222b
scoot404 <thezipcompany@gmail.com>
2022-09-26 18:26:08 -0400

    Add VSTAR/V-UNION to CardType.java

--- model/src/main/java/tcgone/carddb/model/CardType.java
+++ model/src/main/java/tcgone/carddb/model/CardType.java
@@ -60,6 +60,8 @@ public enum CardType {
   VMAX(113, "Pokémon VMAX", "vmax"),
   RAPID_STRIKE(114, "Rapid Strike", "rapid-strike"),
   SINGLE_STRIKE(115, "Single Strike", "single-strike"),
+  VSTAR(116, "Pokémon VSTAR", "vstar"),
+  VUNION(117, "Pokémon V-UNION", "v-union"),
 
   NOT_IMPLEMENTED(201, "Not Implemented", "not-implemented"),
 
