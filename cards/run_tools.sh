de7703407a8ac08d1ff6c7f9fc5b4a00ef077ab2
axpendix <axpendix@hotmail.com>
2023-10-24 22:52:22 +0200

    Update run_tools.sh

--- cards/run_tools.sh
+++ cards/run_tools.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
-mvn -Dexec.mainClass=tcgone.carddb.tools.Application -Dexec.args="--yaml=src/main/resources/cards --run-interactive-merger --export-yaml" -Dexec.classpathScope=test test-compile exec:java
+mvn -Dexec.mainClass=tcgone.carddb.tools.Application \
+-Dexec.args="--yaml=src/main/resources/cards --run-interactive-merger --export-yaml --output-dir=src/main/resources/cards" \
+-Dexec.classpathScope=test \
+test-compile \
+exec:java
